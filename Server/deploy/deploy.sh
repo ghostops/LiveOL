fd85d03c575ddea26dd86be26a750843b692a280
ghostops <ludvig@ghostops.nu>
2020-04-14 10:33:04 +0200

    add env variable

--- Server/deploy/deploy.sh
+++ Server/deploy/deploy.sh
@@ -1,3 +1,3 @@
-docker build -t ${REPO} .
+docker build -t ${REPO} --build-arg EVENTOR_API_KEY=$EVENTOR_API_KEY .
 docker tag ${REPO}:latest 237763290421.dkr.ecr.eu-north-1.amazonaws.com/${REPO}:latest
 docker push 237763290421.dkr.ecr.eu-north-1.amazonaws.com/${REPO}
