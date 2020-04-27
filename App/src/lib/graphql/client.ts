841da76a60c6bbb7503958f51da32d5f0c7fd448
ghostops <ludvig@ghostops.nu>
2020-04-27 10:47:31 +0200

    add mocks

--- App/src/lib/graphql/client.ts
+++ App/src/lib/graphql/client.ts
@@ -4,6 +4,7 @@ import { APP_VERSION, DEVICE_NAME } from 'util/const';
 const userId = `LiveOLApp:${APP_VERSION}:${DEVICE_NAME}`;
 
 export const client = new ApolloClient({
-    uri: 'https://api-liveol.larsendahl.se',
+    // uri: 'https://api-liveol.larsendahl.se',
+    uri: 'https://b5c0b6c3.ngrok.io',
     headers: { userId },
 });
