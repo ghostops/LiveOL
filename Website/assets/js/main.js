718d3e03f460de6b50e43e225ed1f1bf9b38b822
ghostops <ludvig@ghostops.nu>
2020-04-30 15:41:02 +0200

    cleanup

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -1,7 +1,7 @@
 (function ($)
   { "use strict"
   
-/* 1. Proloder */
+/* 1. Preloder */
     $(window).on('load', function () {
       $('#preloader-active').delay(450).fadeOut('slow');
       $('body').delay(450).css({
@@ -86,116 +86,6 @@
     }
     mainSlider();
 
-
- // recent-active
-  $('.customar-active').slick({
-    dots: true,
-    infinite: true,
-    speed: 600,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev"> <span class="flaticon-arrow"></span></button>',
-    nextArrow: '<button type="button" class="slick-next"> <span class="flaticon-arrow"><span></button>',
-    autoplay:false,
-    initialSlide: 3,
-    centerMode: true,
-    loop:true,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        }
-      }, 
-           {
-        breakpoint: 1050,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
-/* 4. Testimonial Active*/
-  var testimonial = $('.h1-testimonial-active');
-    if(testimonial.length){
-    testimonial.slick({
-        dots: false,
-        infinite: true,
-        speed: 1000,
-        autoplay:true,
-        loop:true,
-        arrows: false,
-        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false,
-              arrow:false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false,
-            }
-          }
-        ]
-      });
-    }
-
-
 /* 5.  Applic App */
     var client_list = $('.app-active');
     if(client_list.length){
@@ -209,7 +99,7 @@
         dots: false,
         margin: 15,
         nav  : false,
-        navText : ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+        autoWidth: true,
         autoplayHoverPause: true,
         responsive : {
           0 : {
@@ -228,12 +118,6 @@
       });
     }
 
-
-/* 6. Nice Selectorp  */
-  var nice_Select = $('select');
-    if(nice_Select.length){
-      nice_Select.niceSelect();
-    }
 
 /* 7.  Custom Sticky Menu  */
     $(window).on('scroll', function () {
@@ -279,26 +163,5 @@
     new WOW().init();
 
 /* 11. Datepicker */
-    
-// 11. ---- Mailchimp js --------//  
-    function mailChimp() {
-      $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
-
-
-// 12 Pop Up Img
-    var popUp = $('.single_gallery_part, .img-pop-up');
-      if(popUp.length){
-        popUp.magnificPopup({
-          type: 'image',
-          gallery:{
-            enabled:true
-          }
-        });
-      }
-
-
-
 
 })(jQuery);
