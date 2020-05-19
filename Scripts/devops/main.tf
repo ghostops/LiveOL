5953bec2f5ba817124424c5c85acc8b1c376a4a6
ghostops <ludvig@ghostops.nu>
2020-05-19 10:47:33 +0200

    move to DO for server

--- Scripts/devops/main.tf
+++ Scripts/devops/main.tf
@@ -6,68 +6,25 @@ terraform {
   }
 }
 
-provider "aws" {
-  version = "~> 2.0"
-  region  = "eu-north-1"
+provider "digitalocean" {
+  version = "~> 1.18"
 }
 
-variable "ssh_key_name" {
-  type    = string
-  default = "ghost@ghst"
+resource "digitalocean_droplet" "server" {
+  image  = "ubuntu-20-04-x64"
+  name   = "LiveOL"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [var.ssh_key_fingerprint]
 }
 
-variable "ami_id" {
-  type    = string
-  default = "ami-0b7937aeb16a7eb94"
+resource "digitalocean_project_resources" "linkToProject" {
+  project = var.do_project_id
+  resources = [
+    digitalocean_droplet.server.urn
+  ]
 }
 
-variable "vpc_subnets" {
-  type    = list(string)
-  default = ["subnet-35ba424e", "subnet-6eccd724", "subnet-aa4f91c3"]
+output "droplet_ip" {
+  value = digitalocean_droplet.server.ipv4_address
 }
-
-variable "iam_profile" {
-  type    = string
-  default = "liveol-server-poweruser"
-}
-
-variable "security_groups" {
-  type    = list(string)
-  default = ["sg-04ab20a9b3e5d8478"]
-}
-
-resource "aws_launch_configuration" "liveol_conf" {
-  name_prefix          = "liveol-"
-  image_id             = var.ami_id
-  instance_type        = "t3.micro"
-  spot_price           = "0.0045"
-  iam_instance_profile = var.iam_profile
-  user_data            = file("deployment.tpl")
-  security_groups      = var.security_groups
-  enable_monitoring    = false
-  key_name             = var.ssh_key_name
-
-  root_block_device {
-    volume_type           = "standard"
-    volume_size           = "16"
-    delete_on_termination = true
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "liveol_server" {
-  name_prefix          = "liveol-"
-  vpc_zone_identifier  = var.vpc_subnets
-  launch_configuration = aws_launch_configuration.liveol_conf.name
-  min_size             = 1
-  max_size             = 1
-  desired_capacity     = 1
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
---
+++ Scripts/devops/vars.tf
@@ -0,0 +1,6 @@
+variable "ssh_key_fingerprint" {}
+variable "do_project_id" {}
+
+# not used until after tf
+variable "aws_access_key" {}
+variable "aws_secret_access_key" {}
