42a7ba3ee92cc2a700aacf2111497b08f68a7bd6
Ludvig Larsendahl <ludvig@ghostops.nu>
2020-09-19 16:12:52 +0200

    android build

--- App/android/app/src/main/java/se/liveol/rn/generated/BasePackageList.java
+++ App/android/app/src/main/java/se/liveol/rn/generated/BasePackageList.java
@@ -14,8 +14,10 @@ public class BasePackageList {
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localization.LocalizationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.screenorientation.ScreenOrientationPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage()
