992dba1a2eedd3bff3782907cac9f868cb130e49
Ludvig Larsendahl <ludvig@ghostops.nu>
2020-09-20 19:21:53 +0200

    android permissions again

--- App/android/app/src/main/java/se/liveol/rn/generated/BasePackageList.java
+++ App/android/app/src/main/java/se/liveol/rn/generated/BasePackageList.java
@@ -17,7 +17,6 @@ public class BasePackageList {
         new expo.modules.localization.LocalizationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.screenorientation.ScreenOrientationPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage()
