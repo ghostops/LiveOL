4ac2669a308f5d87dec81ef50ad4527f2cdc0b72
Ludvig Larsendahl <ludvig@ghostops.nu>
2020-10-09 13:19:28 +0200

    fix badge

--- App/src/views/components/result/item/badge.tsx
+++ App/src/views/components/result/item/badge.tsx
@@ -11,11 +11,19 @@ export const OLResultBadge: React.FC<Props> = ({ place }) => (
 	<View
 		style={{
 			alignItems: 'center',
-			justifyContent: 'flex-start',
+			justifyContent: 'center',
 			flex: 1,
 		}}>
 		{Boolean(place.length > 0 && place !== '-') && (
-			<Badge style={{ backgroundColor: COLORS.MAIN, justifyContent: 'center', alignItems: 'center' }}>
+			<Badge
+				style={{
+					backgroundColor: COLORS.MAIN,
+					justifyContent: 'center',
+					alignItems: 'center',
+					width: 25,
+					height: 25,
+					borderRadius: 50,
+				}}>
 				<OLText size={12} font="Proxima_Nova" style={{ color: 'white' }}>
 					{place}
 				</OLText>
