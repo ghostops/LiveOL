81eea42bc31ae0cf070a546ef6843ae9544241ba
Ludvig Larsendahl <ludvig@ghostops.nu>
2021-03-12 15:52:46 +0100

    add emtpy serbian + credits

--- App/src/views/components/lang/flag.tsx
+++ App/src/views/components/lang/flag.tsx
@@ -1,30 +1,40 @@
 import * as React from 'react';
-import { Image } from 'react-native';
+import { Image, ImageStyle } from 'react-native';
 
 interface Props {
 	code: string;
 	size: number;
+	style?: ImageStyle;
 }
 
 const remapCode = {
 	en: 'gb',
 	sv: 'se',
 	no: 'no',
+	sr: 'rs',
 };
 
 const FLAGS = {
 	se: require('../../../../assets/images/flags/se.png'),
 	no: require('../../../../assets/images/flags/no.png'),
 	gb: require('../../../../assets/images/flags/gb.png'),
+	rs: require('../../../../assets/images/flags/rs.png'),
 };
 
-export const OLFlag: React.FC<Props> = ({ code, size }) =>
-	FLAGS[remapCode[code]] ? (
+export const OLFlag: React.FC<Props> = ({ code, size, style }) => {
+	const flag = FLAGS[remapCode[code]];
+
+	if (!flag) {
+		return null;
+	}
+	return (
 		<Image
-			source={FLAGS[remapCode[code]]}
+			source={flag}
 			style={{
 				height: size * 0.65,
 				width: size,
+				...style,
 			}}
 		/>
-	) : null;
+	);
+};
