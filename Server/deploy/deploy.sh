71a9739193172fc230d8f7bf76646482c2573787
Ludvig Larsendahl <ludvig@ghostops.nu>
2021-05-12 12:09:46 +0200

    remove old docker process

--- Server/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-docker build -t ${REPO} --build-arg EVENTOR_API_KEY_SE=$EVENTOR_API_KEY_SE --build-arg EVENTOR_API_KEY_AU=$EVENTOR_API_KEY_AU --build-arg SLACK_WEBHOOK=$SLACK_WEBHOOK .
-docker tag ${REPO}:latest 237763290421.dkr.ecr.eu-north-1.amazonaws.com/${REPO}:latest
-docker push 237763290421.dkr.ecr.eu-north-1.amazonaws.com/${REPO}
