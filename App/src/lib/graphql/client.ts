7b40b465ec77f1c2d5716339a2eb61f98f98613f
Ludvig Larsendahl <ludvig@ghostops.nu>
2021-09-20 12:06:25 +0200

    fix!

--- App/src/lib/graphql/client.ts
+++ App/src/lib/graphql/client.ts
@@ -4,7 +4,6 @@ import { VERSION, DEVICE_NAME } from 'util/const';
 const userId = `LiveOLApp:${VERSION}:${DEVICE_NAME}`;
 
 export const client = new ApolloClient({
-	// uri: 'https://api-liveol.larsendahl.se',
-	uri: 'http://localhost:4000',
+	uri: 'https://api-liveol.larsendahl.se',
 	headers: { userId },
 });
