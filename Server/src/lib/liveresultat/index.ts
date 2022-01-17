cdbbefd78538f2178f67369bc9f18cfd459040cd
Ludvig Larsendahl <ludvig@ghostops.nu>
2022-01-17 14:10:24 +0100

    add scraping “backup” to the liveresultat api

--- Server/src/lib/liveresultat/index.ts
+++ Server/src/lib/liveresultat/index.ts
@@ -1,11 +1,13 @@
-import { Cacher } from 'lib/redis';
-import { getEnv } from 'lib/helpers/env';
-import { LiveresultatApi } from './types';
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { LiveresultatReplayer } from './replay';
-import * as fs from 'fs';
-import * as moment from 'moment';
 import * as ms from 'ms';
+import * as moment from 'moment';
+import * as fs from 'fs';
+import { scrapeAllCompetitions } from './scrape';
+import { LiveresultatReplayer } from './replay';
+import { LiveresultatApi } from './types';
+import { IncomingWebhook } from '@slack/webhook';
+import { getEnv } from 'lib/helpers/env';
+import { Cacher } from 'lib/redis';
 
 const DEV = getEnv('test') === 'true';
 
@@ -19,54 +21,58 @@ export class LiveresultatAPIClient {
 			headers: {
 				'User-Agent': 'LiveOL Server',
 			},
+			baseURL: this.root,
 		});
 	}
 
-	public getcompetitions = async (): Promise<LiveresultatApi.getcompetitions> =>
-		this.cachedRequest(
-			this.client.get(`${this.root}/api.php?method=getcompetitions`),
-			'getcompetitions',
-			'1 minute',
-		);
+	public getcompetitions = async (): Promise<LiveresultatApi.getcompetitions> => {
+		try {
+			return await this.cachedRequest(
+				this.client.get(`/api.php?method=getcompetitions`),
+				'getcompetitions',
+				'1 minute',
+			);
+		} catch (error) {
+			if (error?.message?.includes('JSON at position')) {
+				console.warn(error.message + ': getcompetitions');
+				return await scrapeAllCompetitions(this.client, this.cache);
+			}
+
+			throw error;
+		}
+	};
 
 	public getcompetition = async (id: number): Promise<LiveresultatApi.competition> =>
 		this.cachedRequest(
-			this.client.get(`${this.root}/api.php?method=getcompetitioninfo&comp=${id}`),
+			this.client.get(`/api.php?method=getcompetitioninfo&comp=${id}`),
 			`getcompetition:${id}`,
 			'1 minute',
 		);
 
 	public getclasses = async (id: number): Promise<LiveresultatApi.getclasses> =>
-		this.cachedRequest(
-			this.client.get(`${this.root}/api.php?method=getclasses&comp=${id}`),
-			`getclasses:${id}`,
-			'1 minute',
-		);
+		this.cachedRequest(this.client.get(`/api.php?method=getclasses&comp=${id}`), `getclasses:${id}`, '1 minute');
 
 	public getclassresults = async (id: number, _class: string): Promise<LiveresultatApi.getclassresults> =>
 		this.cachedRequest(
-			this.client.get(
-				`${this.root}/api.php?method=getclassresults&comp=${id}&class=${encodeURIComponent(_class)}`,
-				{
-					headers: {
-						'Content-Type': 'application/json;charset=UTF-8',
-					},
+			this.client.get(`/api.php?method=getclassresults&comp=${id}&class=${encodeURIComponent(_class)}`, {
+				headers: {
+					'Content-Type': 'application/json;charset=UTF-8',
 				},
-			),
+			}),
 			`getclassresults:${id}:${encodeURIComponent(_class)}`,
 			'15 seconds',
 		);
 
 	public getlastpassings = async (id: number): Promise<LiveresultatApi.lastpassings> =>
 		this.cachedRequest(
-			this.client.get(`${this.root}/api.php?method=getlastpassings&comp=${id}`),
+			this.client.get(`/api.php?method=getlastpassings&comp=${id}`),
 			`getlastpassings:${id}`,
 			'15 seconds',
 		);
 
 	public getclubresults = async (id: string, club: string): Promise<LiveresultatApi.getclubresults> => {
 		return this.cachedRequest(
-			this.client.get(`${this.root}/api.php?method=getclubresults&comp=${id}&club=${encodeURIComponent(club)}`),
+			this.client.get(`/api.php?method=getclubresults&comp=${id}&club=${encodeURIComponent(club)}`),
 			`getclubresults:${id}:${encodeURIComponent(club)}`,
 			'15 seconds',
 		);
---
+++ Server/src/lib/liveresultat/scrape.ts
@@ -0,0 +1,40 @@
+import * as cheerio from 'cheerio';
+import * as ms from 'ms';
+import { LiveresultatApi } from './types';
+import { Cacher } from 'lib/redis';
+import { AxiosInstance } from 'axios';
+
+export const scrapeAllCompetitions = async (
+	client: AxiosInstance,
+	cache: Cacher,
+): Promise<LiveresultatApi.getcompetitions> => {
+	let data: LiveresultatApi.competition[] = await cache.get('getcompetitionsScrape');
+
+	if (!data) {
+		data = [];
+
+		const res = await client.get('/');
+		const $ = cheerio.load(res.data);
+
+		$('#tblComps tbody tr').each((i, el) => {
+			if (i === 0) return;
+
+			const idRegex = new RegExp(/comp=?(.*?)&/g);
+
+			const date = el.children?.[0].children?.[0].data || '';
+
+			const idExec = idRegex.exec(el.children?.[2].children?.[0]?.attribs?.href);
+			const id = Number(idExec?.[1]) || 0;
+
+			const name = el.children?.[2].children?.[0].children?.[0]?.data || '';
+
+			const organizer = el.children?.[4]?.children?.[0]?.data || '';
+
+			data.push({ date, name, id, organizer, timediff: 0 });
+		});
+
+		cache.set('getcompetitionsScrape', data, ms('1 minute'));
+	}
+
+	return { competitions: data };
+};
