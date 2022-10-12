9963e218f6d270f7dfde92fe7be6d0774a941bf7
Ludvig Larsendahl <ludvig@ghostops.nu>
2022-10-12 11:07:15 +0200

    persist stores

--- App/src/store/audio.ts
+++ App/src/store/audio.ts
@@ -1,13 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type AudioState = {
   isMuted: boolean;
   setMuted: (value: boolean) => void;
 };
 
-export const useAudioStore = create<AudioState>(set => ({
-  isMuted: false,
-  setMuted(value) {
-    set(() => ({ isMuted: value }));
-  },
-}));
+export const useAudioStore = create<AudioState>()(
+  persist(
+    set => ({
+      isMuted: false,
+      setMuted(value) {
+        set(() => ({ isMuted: value }));
+      },
+    }),
+    { name: '@liveol/audio', getStorage: () => AsyncStorage },
+  ),
+);
