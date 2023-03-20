c2f7a5a54c9cb4c35a184c87d371def69cee656a
Ludvig L <ludvig@ghostops.nu>
2023-03-20 15:42:42 +0100

    promo self component

--- App/src/views/scenes/home/component.tsx
+++ App/src/views/scenes/home/component.tsx
@@ -8,11 +8,8 @@ import { TodaysCompetitions } from 'views/components/home/today';
 import { View, TouchableOpacity } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { OlCompetition } from 'lib/graphql/generated/types';
-import { useOLNavigation } from 'hooks/useNavigation';
-import { OLButton } from 'views/components/button';
 import { useTheme } from 'hooks/useTheme';
-import { usePromoStore } from 'store/promo';
-import { useIap } from 'lib/iap';
+import { OLHomePromo } from './promo';
 
 interface Props {
   competitions: OlCompetition[];
@@ -25,72 +22,6 @@ interface Props {
   refetch: () => Promise<void>;
   landscape?: boolean;
 }
-
-const OLHomePromo: React.FC = () => {
-  const { px, colors } = useTheme();
-  const { navigate } = useOLNavigation();
-  const { displayPromo, setDisplayPromo } = usePromoStore();
-  const { plusActive } = useIap();
-
-  if (plusActive) {
-    return null;
-  }
-
-  if (!displayPromo) {
-    return (
-      <TouchableOpacity
-        style={{ backgroundColor: colors.DARK, paddingVertical: px(8) }}
-        onPress={() => navigate('Plus')}
-      >
-        <OLText bold size={16} style={{ color: 'white', textAlign: 'center' }}>
-          LiveOL+
-        </OLText>
-      </TouchableOpacity>
-    );
-  }
-
-  return (
-    <View
-      style={{
-        backgroundColor: 'white',
-        padding: px(16),
-        borderWidth: 4,
-        borderColor: colors.MAIN,
-        borderRadius: 8,
-        margin: px(8),
-      }}
-    >
-      <OLText
-        bold
-        italics
-        size={22}
-        style={{ textAlign: 'center', marginBottom: px(8) }}
-      >
-        LiveOL+
-      </OLText>
-
-      <OLText size={16} style={{ textAlign: 'center', marginBottom: px(16) }}>
-        Get the most out of LiveOL and support its continued development. Check
-        out LiveOL+ today:
-      </OLText>
-
-      <OLButton small onPress={() => navigate('Plus')}>
-        Check it out
-      </OLButton>
-
-      <TouchableOpacity
-        style={{ marginTop: px(16) }}
-        onPress={() => {
-          setDisplayPromo(false);
-        }}
-      >
-        <OLText size={14} style={{ textAlign: 'center' }}>
-          Close
-        </OLText>
-      </TouchableOpacity>
-    </View>
-  );
-};
 
 export const OLHome: React.FC<Props> = ({
   onCompetitionPress,
