f44098247aaef901c9337a04f166b792e6c8f0d7
Ludvig L <ludvig@ghostops.nu>
2023-10-02 14:21:45 +0200

    bump rn

--- App/android/app/src/main/java/se/liveol/rn/MainActivity.java
+++ App/android/app/src/main/java/se/liveol/rn/MainActivity.java
@@ -2,9 +2,10 @@ package se.liveol.rn;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import android.os.Bundle;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import android.os.Bundle;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -19,46 +20,24 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-   * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
-   * (Paper).
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 
-  public static class MainActivityDelegate extends ReactActivityDelegate {
-    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-      super(activity, mainComponentName);
-    }
-
-    @Override
-    protected ReactRootView createRootView() {
-      ReactRootView reactRootView = new ReactRootView(getContext());
-      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-      return reactRootView;
-    }
-
-    @Override
-    protected boolean isConcurrentRootEnabled() {
-      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-    }
-
-    // https://reactnavigation.org/docs/getting-started/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
-    }
-  }
-
-  // Hack to make splash screen work kind of
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // react-native-bootsplash
     RNBootSplash.init(this);
+    
     super.onCreate(null);
   }
 }
--- App/src/views/components/codepush/index.tsx
+++ App/src/views/components/codepush/index.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import codePush from 'react-native-code-push';
 
-type Props = {
-  children: any;
-};
-
-class OLCodePushContainer extends React.Component<Props> {
-  render() {
-    return this.props.children;
-  }
-}
-
-const OLCodePushWrapper = codePush(OLCodePushContainer);
-
-export const OLCodePush: React.FC<Props> = ({ children }) => {
+export const OLCodePush: React.FC<any> = ({ children }) => {
   if (__DEV__) {
     return children;
   }
 
-  return <OLCodePushWrapper>{children}</OLCodePushWrapper>;
+  return <>{children}</>;
 };
