38093ed29d462a88810adced4fd132368cede950
Ludvig L <ludvig@ghostops.nu>
2024-02-10 22:07:36 +0100

    last passing status

--- App/src/views/components/competition/lastPassing.tsx
+++ App/src/views/components/competition/lastPassing.tsx
@@ -1,12 +1,14 @@
 import { TRPCQueryOutput } from '~/lib/trpc/client';
 import { OLText } from '../text';
+import { useStatusI18n } from '~/hooks/useStatusI18n';
 
 type Props = {
   result: TRPCQueryOutput['getCompetitionLastPassings'][0];
 };
 
 export const OLLastPassing: React.FC<Props> = ({ result }) => {
-  const timeStr = result.time.includes(':') ? result.time : 'X';
+  const statusText = useStatusI18n(result.status);
+  const timeStr = result.status === 0 ? result.time : statusText;
 
   return (
     <OLText size={16} style={{ color: 'white' }}>
--- Server/src/lib/marshall/passings.ts
+++ Server/src/lib/marshall/passings.ts
@@ -7,11 +7,46 @@ export interface IOLPassing {
   controlName: string;
   passtime: string;
   runnerName: string;
-  // is sometimes a string, but should be an int for successful checks
   time: string;
+  status: number;
 }
 
 export const marshallPassing = (res: LiveresultatApi.passing): IOLPassing => {
+  let status = 0;
+
+  if (!res.time.includes(':')) {
+    switch (res.time) {
+      case 'ej start':
+        status = 1;
+        break;
+      case 'utgått':
+        status = 2;
+        break;
+      case 'återbud':
+        status = 11;
+        break;
+      case 'uppflyttad':
+        status = 12;
+        break;
+      case 'ej startat':
+        status = 10;
+        break;
+      case 'felst.':
+        status = 3;
+        break;
+      case 'diskv.':
+        status = 4;
+        break;
+      case 'ö. maxtid':
+        status = 5;
+        break;
+      case 'godkänd':
+      default:
+        status = 0;
+        break;
+    }
+  }
+
   return {
     id: `${res.class.replace(/ /g, '_')}:${res.runnerName.replace(/ /g, '_')}:${res.time.replace(/ /g, '_')}`.toLowerCase(),
     class: res.class,
@@ -20,5 +55,6 @@ export const marshallPassing = (res: LiveresultatApi.passing): IOLPassing => {
     passtime: res.passtime,
     runnerName: res.runnerName,
     time: res.time,
+    status,
   };
 };
