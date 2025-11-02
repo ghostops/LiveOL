2e0f3623088aa0f1a547cef78bdeb6b9cfa904a8
Ludvig L <ludvig@ghostops.nu>
2025-11-02 21:20:25 +0100

    txt

--- App/src/views/scenes/competitionv2/index.tsx
+++ App/src/views/scenes/competitionv2/index.tsx
@@ -6,11 +6,13 @@ import { $api } from '~/lib/react-query/api';
 import { OLText } from '~/views/components/text';
 import { useOLNavigation } from '~/hooks/useNavigation';
 import { useLayoutEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const OLSceneCompetitionV2 = () => {
   const { colors } = useTheme();
   const { params } = useRoute<RouteProp<RootStack, 'CompetitionV2'>>();
   const navigation = useOLNavigation();
+  const { t } = useTranslation();
 
   const getCompetitionQuery = $api.useQuery('get', '/v2/competitions/{id}', {
     params: {
@@ -30,12 +32,73 @@ export const OLSceneCompetitionV2 = () => {
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: colors.WHITE,
       }}
     >
-      <OLText>{getCompetitionQuery.data?.data.competition.name}</OLText>
+      <View style={{ padding: 8 }}>
+        {getCompetitionQuery.data?.data.competition.organizer && (
+          <OLText size={16}>
+            {t('competitions.organizedBy')}:{' '}
+            {getCompetitionQuery.data.data.competition.organizer}
+          </OLText>
+        )}
+      </View>
+      {getCompetitionQuery.data?.data.competition && (
+        <>
+          <OLText>
+            OL Organization ID:{' '}
+            {getCompetitionQuery.data.data.competition.olOrganizationId}
+          </OLText>
+          <OLText>
+            Name: {getCompetitionQuery.data.data.competition.name}
+          </OLText>
+          <OLText>
+            Date: {getCompetitionQuery.data.data.competition.date}
+          </OLText>
+          <OLText>
+            Eventor ID:{' '}
+            {getCompetitionQuery.data.data.competition.eventorId || 'N/A'}
+          </OLText>
+          <OLText>
+            Distance:{' '}
+            {getCompetitionQuery.data.data.competition.distance || 'N/A'}
+          </OLText>
+          <OLText>
+            Punch System:{' '}
+            {getCompetitionQuery.data.data.competition.punchSystem || 'N/A'}
+          </OLText>
+          <OLText>
+            Notification:{' '}
+            {getCompetitionQuery.data.data.competition.notification || 'N/A'}
+          </OLText>
+          <OLText>
+            Country Code:{' '}
+            {getCompetitionQuery.data.data.competition.countryCode || 'N/A'}
+          </OLText>
+          <OLText>
+            Has Live Data:{' '}
+            {getCompetitionQuery.data.data.competition.hasLiveData
+              ? 'Yes'
+              : 'No'}
+          </OLText>
+          <OLText>
+            Has Eventor Data:{' '}
+            {getCompetitionQuery.data.data.competition.hasEventorData
+              ? 'Yes'
+              : 'No'}
+          </OLText>
+          <OLText>Links:</OLText>
+          {getCompetitionQuery.data.data.competition.links.map(
+            (link, index) => (
+              <OLText key={index}>
+                {link.text}: {link.href}
+              </OLText>
+            ),
+          )}
+        </>
+      )}
+
+      {/* Add a map view based on lat/lng of competition */}
     </View>
   );
 };
